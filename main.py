a32c33fa85d5a136d0f6a6d7863ed87911571e6f
unknown <revathikuntrapakam16@gmail.com>
2022-03-22 09:53:49 +0530

    first midification

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running and I am changing something."
+    return "Flask app is running and I am changing something (Whatever changes can be done in main can be visible in heroku app)"
 
 port = int(os.getenv("PORT", 5001))
 
